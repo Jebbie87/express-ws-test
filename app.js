152ce71283751694ec5a669012fa28d0db5879bc
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-11 17:33:45 -0400

    Added some websocket routes

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
+'use strict'
+
 const express = require('express')
 const app = express()
 const expressWs = require('express-ws')(app)
 
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   console.log('middleware')
   req.testing = 'testing'
   return next()
 })
 
-app.get('/', function (req, res, next) {
+app.get('/', (req, res, next) => {
   console.log('get route', req.testing)
   res.end()
 })
 
-app.ws('/', function (ws, req) {
-  ws.on('message', function(msg) {
+app.ws('/', (ws, req) => {
+  ws.on('connection', () => {
+    console.log('WebSocket was opened')
+  })
+
+  ws.on('message', (msg) => {
     console.log('msg', msg)
+
   })
   console.log('socket', req.testing)
+
+  ws.on('close', () => {
+    console.log('WebSocket was closed')
+  })
 })
 
-app.listen(process.env.PORT || 8080, function listening() {
+consolewsInstance.getWss().clients
+
+app.listen(process.env.PORT || 8080, () => {
   console.log('Server listening on port 5000')
 })
