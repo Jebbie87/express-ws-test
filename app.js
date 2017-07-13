ab461bb2334e6849bd8974c5814c4cc13e95ec78
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-13 19:51:27 -0400

    Added basic twilio

--- app.js
+++ app.js
@@ -1,108 +1,12 @@
 'use strict'
 
-// const express = require('express')
-// const app = express()
-// const expressWs = require('express-ws')(app)
-
-
-// app.use((req, res, next) => {
-//   console.log('middleware')
-//   req.testing = 'testing'
-//   return next()
-// })
-
-// app.get('/', (req, res, next) => {
-//   console.log('get route', req.testing)
-//   res.end()
-// })
-
-// app.ws('/', (ws, req) => {
-//   console.log('WebSocket was opened')
-//   ws.on('connection', () => {
-//     console.log('WebSocket was opened')
-//   })
-
-//   ws.on('message', (msg) => {
-//     ws.send(msg)
-//     console.log('msg', msg)
-
-//   })
-//   console.log('socket', req.testing)
-
-//   ws.on('close', () => {
-//     console.log('WebSocket was closed')
-//   })
-// })
-
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log('Server listening on port 5000')
-// })
-
-// const express = require('express');
-// const WebSocket = require('ws').Server;
-
-// const server = express()
-//   .listen(process.env.PORT || 8080, () => console.log('Listening on %d', server.address().port));
-
-// // app.use(function (req, res) {
-// //   res.send({ msg: "hello" });
-// // });
-
-// // const server = http.createServer(app);
-// const wss = new WebSocket({ server });
-
-// server.get('/', )
-// wss.on('connection', function connection(ws, req) {
-//   console.log('connected')
-//   const location = url.parse(req.url, true);
-//   // You might use location.query.access_token to authenticate or share sessions
-//   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-//   wss.broadcast = data => {
-//     console.log('broadcasting')
-//     wss.clients.forEach(client => {
-//       client.send(JSON.stringify(data))
-//     })
-//   }
-
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//     wss.broadcast({msg: 'testing broadcasting message'})
-//   });
-
-//   // ws.send('something');
-
-//   ws.on('close', () => {
-//     wss.broadcast({msg: 'testing broadcasting close'})
-//     console.log('client disconnect')
-//   })
-// });
-
-// server.listen(process.env.PORT || 8080, function listening() {
-//   console.log('Listening on %d', server.address().port);
-// });
-
-// var app = require('express')();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-
-// server.listen(process.env.PORT || 80);
-
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + '/index.html');
-// });
-
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-
-
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http')
+const bodyParser = require('body-parser')
+
+const router = require('./src/router')
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
---
+++ config.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv')
+const cfg = {}
+
+cfg.port = process.env.PORT || 3000
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+//
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+
+cfg.twimlAppSid = process.env.TWILIO_TWIML_APP_SID;
+cfg.callerId = process.env.TWILIO_CALLER_ID;
+
+// Export configuration object
+module.exports = cfg;
