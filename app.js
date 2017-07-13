147d8f342beaf15f9b16ca0f34406519712b13ad
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-13 19:52:29 -0400

    Added another use for express server

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
+  .use(bodyParser.urlencoded({extended: false}))
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
