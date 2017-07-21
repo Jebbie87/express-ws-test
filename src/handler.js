3f738ff132a81781efc1d0cca820ed5fc3aee7fe
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-07-21 09:38:13 -0400

    Added back token generator

--- src/handler.js
+++ src/handler.js
@@ -6,37 +6,25 @@ const config = require('../config');
 
 console.log('config', config)
 
-var accountSid = 'AC984ccb6a7e8ea07a136da8d48486c3cd';
-var authToken = '42bca67f223524876526749b5fb6cf88';
-console.log('test')
-var client = require('twilio')(accountSid, authToken);
+exports.tokenGenerator = () => {
+  const identity = nameGenerator();
+  const capability = new ClientCapability({
+    accountSid: config.accountSid,
+    authToken: config.authToken,
+  });
 
-client.sip.ipAccessControlLists.create({
-    friendlyName: "My new acl 1"
-}, function(err, ipAccessControlList) {
-  console.log(ipAccessControlList)
-    // process.stdout.write(ipAccessControlList.sid);
-});
+  capability.addScope(new ClientCapability.IncomingClientScope(identity));
+  capability.addScope(new ClientCapability.OutgoingClientScope({
+    applicationSid: config.twimlAppSid,
+    clientName: identity,
+  }));
 
-// exports.tokenGenerator = function tokenGenerator() {
-//   const identity = nameGenerator();
-//   const capability = new ClientCapability({
-//     accountSid: config.accountSid,
-//     authToken: config.authToken,
-//   });
-
-//   capability.addScope(new ClientCapability.IncomingClientScope(identity));
-//   capability.addScope(new ClientCapability.OutgoingClientScope({
-//     applicationSid: config.twimlAppSid,
-//     clientName: identity,
-//   }));
-
-//   // Include identity and token in a JSON response
-//   return {
-//     identity: identity,
-//     token: capability.toJwt(),
-//   };
-// };
+  // Include identity and token in a JSON response
+  return {
+    identity: identity,
+    token: capability.toJwt(),
+  };
+};
 
 // exports.voiceResponse = function voiceResponse(toNumber) {
 //   // Create a TwiML voice response
